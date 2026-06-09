f33a6ad65c788b93cac2d6f46631dd40b2f738ea
Sabin Tandukar <ozzyo3839@gmail.com>
2026-06-09 12:21:47 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./app.css";
+import { QRCodeSVG } from "qrcode.react";
+
+function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      name: "Momo",
+      price: 120,
+    },
+    {
+      id: 2,
+      name: "Sausage",
+      price: 40,
+    },
+    {
+      id: 3,
+      name: "KeemaNoodles",
+      price: 150,
+    },
+    {
+      id: 4,
+      name: "Laphing",
+      price: 70,
+    },
+    {
+      id: 5,
+      name: "CornDog",
+      price: 130,
+    },
+    {
+      id: 6,
+      name: "PaniPuri",
+      price: 50,
+    },
+  ]);
+
+  return (
+    <div>
+      <h1>Jhigu Cafe</h1>
+      <h2>Items we provide here</h2>
+      {items.map((item) => (
+        <div className="div-list">
+          <span className="name">{item.name}</span>
+          <span className="price">{item.price}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
